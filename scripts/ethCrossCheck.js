ed22762f16e4b435664d0825e65e8c2faa4a2bab
Veronica <veronica.21.coutts@gmail.com>
2021-11-10 00:30:58 +0200

    Checks addresses with non-zero balance on weth list

--- scripts/ethCrossCheck.js
+++ scripts/ethCrossCheck.js
@@ -17,7 +17,25 @@ const provider = new ethers.providers.JsonRpcProvider(
 async function run() {
   console.log("running");
 
-  
+  var addressesJson = require("../data/ethCrossCheck/verifiedContractAddresses.json");
+  let verifiedContractAddresses = addressesJson.verified_contracts;
+  var wethAddressesJson = require("../data/ethCrossCheck/wethAddresses.json");
+  let wethAddresses = wethAddressesJson.known_weth_addresses;
+  let caseFormatted = new Array();
+
+  for (let i = 0; i < wethAddresses.length; i++) {
+    caseFormatted[i] = wethAddresses[i].toUpperCase();
+  }
+
+  for (let i = 0; i < verifiedContractAddresses.length; i++) {
+    let balance = await provider.getBalance(verifiedContractAddresses[i]);
+    if(balance.toString() != "0") {
+      if(!caseFormatted.includes(verifiedContractAddresses[i].toUpperCase())) {
+        console.log("Not included: ", verifiedContractAddresses[i]);
+        console.log("Eth balance: ", balance.toString())
+      }
+    }
+  }  
 }
 
 function main() {
